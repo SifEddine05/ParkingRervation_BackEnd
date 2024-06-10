d3ae6665cd68a393dbf9f20257fe7d827f5cf84a
SifEddine05 <sifousamir72@gmail.com>
2024-06-10 13:12:37 +0100

    add notification

--- services/firebase.js
+++ services/firebase.js
@@ -2,7 +2,8 @@ const admin = require('firebase-admin');
 const serviceAccount = require('./tdm-project-904da-firebase-adminsdk-4ghdn-f8f7fe9bb2.json'); // Replace with the path to your service account key file
 
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://tdm-project-904da-default-rtdb.firebaseio.com"
 });
 
 module.exports = admin;
